1a4074e75a1b117b84b1e4c456500c7499e1d35f
406-15-suriyapong <54381@yupparaj.ac.th>
2026-06-26 10:43:18 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title(" ✨ My Digital Profile " )
+st.header("🧩 แนะนำตัว")
+st.write("นายสุริยะพงษ์ อดิเรกธรรมากร")
+st.write("ชื่อเล่น ต้นบุญ")
+st.write("เลขที่15 ม.4/6")
+st.header(" 🎯 เป้าหมายและสิ่งที่อยากทำในอนาคต ")
+st.write("เป้าหมาย : **อยากเรียนด้านคอมพิวเตอร์และเทคโนโลยี**")
+st.write("สิ่งที่อยากเป็นในอนาคต: ซอฟต์แวร์เอนจิเนียร์ (Software Engineer)")
